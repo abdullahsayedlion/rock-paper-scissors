16c2f2a75a78113a921b8c571018d906ecb12784
abdullahsayedlion <abdullahsayed5861@gmail.com>
2023-06-08 19:11:05 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomInt = Math.floor(Math.random() * 3);
+  return choices[randomInt];
+}
+
+console.log(getComputerChoice());
