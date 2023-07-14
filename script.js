1a4280976d7cc1ec4b5f94e5cf6d11598c030fcf
abdullahsayedlion <abdullahsayed5861@gmail.com>
2023-07-14 18:44:36 +0100

    created skeleton layout for multiplayer

--- script.js
+++ script.js
@@ -1,61 +1,65 @@
-// get computer's choice
-// get the player's choice
-// determine result
-//   cChoice==pChoice => draw
-//   pChoice=='Rock' and cChoice=='Scissors' => p wins
-//   pChoice=='Scissors' and cChoice=='paper' => p wins
-//   pChoice=='paper' and cChoice=='rock' => p wins
-//   otherwise c has won
-// report the winner
-// increment the winner score
-// play the game five times
-// report the result
+let firstPlayerName;
+let secondPlayerName;
+let drawScore = 0;
+let firstPlayerScore = 0;
+let secondPlayerScore = 0;
 
-let playerScore = 0;
-let computerScore = 0;
-
-function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  const randomInt = Math.floor(Math.random() * 3);
-  return choices[randomInt];
+function getPlayerName(message) {
+  return prompt(message);
 }
 
-//console.log(getComputerChoice());
-
-function getPlayerChoice() {
-  return prompt("Enter your choice(rock, paper, scissors");
+function getPlayerChoice(message) {
+  return prompt(message);
 }
-
-//console.log(getPlayerChoice())
 
 function playRound() {
-  const playerChoice = getPlayerChoice();
-  const computerSelection = getComputerChoice();
-  if (playerChoice === computerSelection) {
+  const firstPlayerChoice = getPlayerChoice(
+    "Player: " +
+      firstPlayerName +
+      " Enter your choice: 1 for rock, 2 for paper, 3 for scissors"
+  );
+  const secondPlayerChoice = getPlayerChoice(
+    "Player: " +
+      secondPlayerName +
+      " Enter your choice: 1 for rock, 2 for paper, 3 for scissors"
+  );
+
+  //get and save the result
+
+  if (firstPlayerChoice === secondPlayerChoice) {
+    drawScore++;
     return "It's a draw";
-  } else if (playerChoice === "rock" && computerSelection === "scissors") {
-    playerScore++;
-    return "You win";
-  } else if (playerChoice === "scissors" && computerSelection === "paper") {
-    playerScore++;
-    return "You win";
-  } else if (playerChoice === "paper" && computerSelection === "rock") {
-    playerScore++;
-    return "You win";
+  } else if (firstPlayerChoice === "1" && secondPlayerChoice === "2") {
+    firstPlayerScore++;
+    return "firstPlayer wins";
+  } else if (firstPlayerChoice === "2" && secondPlayerChoice === "1") {
+    firstPlayerScore++;
+    return "firstPlayer wins";
+  } else if (firstPlayerChoice === "1" && secondPlayerChoice === "3") {
+    firstPlayerScore++;
+    return "firstPlayer wins";
+  } else if (firstPlayerChoice === "3" && secondPlayerChoice === "2") {
+    firstPlayerScore++;
+    return "firstPlayer wins";
   } else {
-    computerScore++;
-    return "You lose";
+    secondPlayerScore++;
+    return "secondPlayer wins";
   }
+
+  //report the result of current round
 }
 
-function game() {
-  console.log(playRound());
-  console.log(playRound());
-  console.log(playRound());
-  console.log(playRound());
-  console.log(playRound());
-  console.log(`Player wins ${playerScore} times`);
-  console.log(`Computer wins ${computerScore} times`);
-}
+function gameLoop() {
+  firstPlayerName = getPlayerName("Enter the first player's name");
+  secondPlayerName = getPlayerName("Enter the second player's name");
+  while (true) {
+    playRound();
+    const nextRound = prompt("Enter exit to stop playing: ");
+    if (nextRound === "exit") break;
+  }
 
-game();
+  //report the result of the game
+  console.log(
+    `firstPlayerName wins ${firstPlayerScore} times + secondPlayerName wins ${secondPlayerScore} times`
+  );
+}
